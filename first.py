275a386d552024a77fcf8351d990ae320d1fb639
Shantnu <shantnu@users.noreply.github.com>
2017-05-18 17:07:31 +0100

    first example

---
+++ first.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pdb
+
+# R matrix
+R = np.matrix([ [-1,-1,-1,-1,0,-1],
+        [-1,-1,-1,0,-1,100],
+        [-1,-1,-1,0,-1,-1],
+        [-1,0,0,-1,0,-1],
+        [0,-1,-1,0,-1,100],
+        [-1,0,-1,-1,0,100] ])
+
+# Q matrix
+Q = np.matrix(np.zeros([6,6]))
+
+
+def get_available_actions(state):
+    current_state = R[state]
+
+    available_actions = np.where(current_state >= 0)[1]
+    return available_actions
+
+def choose_random_action(available_actions):
+    next_action = np.random.choice(available_actions, 1)
+    return next_action
+
+
+for _ in range(1000):
+    while (True):
+        #pdb.set_trace()
+
+
+        initial_state = np.random.choice(6, 1)
+
+        available_actions = get_available_actions(initial_state)
+
+        next_action = choose_random_action(available_actions)
+
+
+        #print(next_action)
+
+        next_states = get_available_actions(next_action)
+
+        #print(Q[5,next_states])
+
+        Q[initial_state, next_action] = R[initial_state, next_action] + ( 0.8 * np.max(Q[next_action,next_states]))
+
+        if next_action == 5:
+            break
+
+Q = Q / np.max(Q) * 100
+print(Q)
+
+
+
+## Test
+
+current_state = 0
+print("\nStarting at: ",current_state)
+
+while current_state != 5:
+    #pdb.set_trace()
+    #print(current_state)
+    next_step = np.argmax(Q[current_state])
+    print("\n Next Step: ",next_step)
+
+    current_state = next_step
+
+current_state = 2
+print("\nStarting at: ",current_state)
+
+while current_state != 5:
+    #pdb.set_trace()
+    #print(current_state)
+    next_step = np.argmax(Q[current_state])
+    print("\n Next Step: ",next_step)
+
+    current_state = next_step
